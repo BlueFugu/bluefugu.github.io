03660c871b5b1ce4f179826055a42bdec28bacdd
BlueFugu <93423314+BlueFugu@users.noreply.github.com>
2022-09-11 13:52:53 +0300

    Add files via upload

---
+++ t6/z2.js
@@ -0,0 +1,9 @@
+/*let name = `Петро`;
+let la = `математика`;
+let otinka = [2, 6, 7, 10];
+console.log(`Вітаю ${name}! Кількість оцінок з предмету ${la} рівна ${otinka.length}. Остання оцінка - ${otinka[otinka.length -1]}`)*/
+let x = [];
+for(i=1; i<6; i++){
+    x[i] = prompt(`Яка у вас оцінка з математики?`)
+}
+console.log(x)
