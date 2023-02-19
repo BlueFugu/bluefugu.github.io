3b4a2a3b4d3e8ed9de4c252c9a9c471f97694c5d
BlueFugu <93423314+BlueFugu@users.noreply.github.com>
2023-02-19 13:15:52 +0200

    Delete JavaScript/t26 directory

--- JavaScript/t26/z2.js
+++
@@ -1,8 +0,0 @@
-function drawDevelopers(){
-    let list = document.getElementById('list');
-    let top_list = '';
-    companies.map(function(company){
-        top_list += `<a href="#" class="list-group-item list-group-item-action">${company.name}</a>`;
-    })
-    list.innerHTML = top_list;
-}drawDevelopers()
